396ef119f8b22fa49e29bc737516e5b291288c69
Tim Mitra <tim@it-guy.com>
2023-12-21 23:59:26 -0500

    added  DragGesture to move ball, added TapGesture to add linearVelocity to the ball, added a Goal target, subscribed to a collision between goal and ball

--- 01-lesson-title/Final/QHoops/QHoops/ImmersiveView.swift
+++ 01-lesson-title/Final/QHoops/QHoops/ImmersiveView.swift
@@ -10,13 +10,47 @@ import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
+  @State private var goal: Entity?
+  @State private var goalScored: EventSubscription?
   var body: some View {
     RealityView { content in
       // Add the initial RealityKit content
       if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
         content.add(scene)
+        /* Occluded floor */
+        let floor = ModelEntity(mesh: .generatePlane(width: 100, depth: 100), materials: [OcclusionMaterial()])
+        floor.generateCollisionShapes(recursive: false)
+        floor.components[PhysicsBodyComponent.self] = .init(
+          massProperties: .default,
+          mode: .static
+        )
+        content.add(floor)
+        // detect a goal
+        goal = content.entities.first?.findEntity(named: "Goal")
+        goalScored = content.subscribe(to: CollisionEvents.Began.self, on: goal) { collisionEvent in
+          print("Goal detected \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
+        }
       }
     }
+    .gesture(dragGesture)
+    .gesture(tapGesture)
+  }
+  var dragGesture: some Gesture {
+    DragGesture()
+      .targetedToAnyEntity()
+      .onChanged { value in
+        value.entity.position = value.convert(value.location3D, from: .local, to: value.entity.parent!)
+        value.entity.components[PhysicsBodyComponent.self]?.mode = .kinematic
+      }
+  }
+  var tapGesture: some Gesture {
+    TapGesture()
+      .targetedToAnyEntity()
+      .onEnded { value in
+        // do nothing
+        value.entity.components[PhysicsBodyComponent.self]?.mode = .dynamic
+        value.entity.components[PhysicsMotionComponent.self]?.linearVelocity = [0,-20,-5]
+      }
   }
 }
 
