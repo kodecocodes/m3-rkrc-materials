afb22c4fde501a243e3043dbd4151a8c653de3e6
Tim Mitra <tim@it-guy.com>
2024-02-11 10:50:34 -0500

    set up part 2 Starter

---
+++ 02-lesson-realitykit-part-two/Final/AmazeMe/AmazeMe/ContentView.swift
@@ -0,0 +1,80 @@
+/// Copyright (c) 2023 Kodeco Inc.
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    @State private var showImmersiveSpace = false
+    @State private var immersiveSpaceIsShown = false
+
+    @Environment(\.openImmersiveSpace) var openImmersiveSpace
+    @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
+
+    var body: some View {
+        VStack {
+            VStack (spacing: 12) {
+                Toggle("Show ImmersiveSpace", isOn: $showImmersiveSpace)
+                    .font(.headline)
+                    .fontWeight(.black)
+                    .foregroundColor(.yellow)
+            }
+            .frame(width: 360)
+            .padding(36)
+            .glassBackgroundEffect()
+        }
+        .onChange(of: showImmersiveSpace) { _, newValue in
+            Task {
+                if newValue {
+                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
+                    case .opened:
+                        immersiveSpaceIsShown = true
+                    case .error, .userCancelled:
+                        fallthrough
+                    @unknown default:
+                        immersiveSpaceIsShown = false
+                        showImmersiveSpace = false
+                    }
+                } else if immersiveSpaceIsShown {
+                    await dismissImmersiveSpace()
+                    immersiveSpaceIsShown = false
+                }
+            }
+        }
+    }
+}
+
+#Preview(windowStyle: .volumetric) {
+    ContentView()
+}
---
+++ 02-lesson-realitykit-part-two/Final/AmazeMe/AmazeMe/ImmersiveView.swift
@@ -0,0 +1,105 @@
+/// Copyright (c) 2023 Kodeco Inc.
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(scene)
+              
+              /* Occluded floor */
+              let floor = ModelEntity(mesh: .generatePlane(width: 100, depth: 100), materials: [OcclusionMaterial()])
+              floor.generateCollisionShapes(recursive: false)
+              floor.components[PhysicsBodyComponent.self] = .init(
+                massProperties: .default,
+                mode: .static
+              )
+              content.add(floor)
+              
+              /* steel ball */
+              let ball = ModelEntity(
+                mesh: .generateSphere(radius: 0.1),
+                materials: [SimpleMaterial(color: .white, isMetallic: true)])
+              ball.position.y = 1.0 // 1 meter (m) above the floor
+              ball.position.z = -1.5 // 1.5m in front of the user
+              ball.position.x = 0.5 // 0.5m right of center
+              ball.generateCollisionShapes(recursive: false)
+              // Enable interactions on the entity.
+              ball.components.set(InputTargetComponent())
+              ball.components.set(CollisionComponent(shapes: [.generateSphere(radius: 0.1)]))
+              // gravity to PhysicsBody
+              ball.components[PhysicsBodyComponent.self] = .init(
+                PhysicsBodyComponent(
+                  // mass in kilograms
+                  massProperties: .init(mass: 5.0),
+                  material: .generate(
+                    staticFriction: 0.0,
+                    dynamicFriction: 0.0,
+                    restitution: 0.5
+                  ),
+                  mode: .dynamic
+                )
+              )
+              // editor - need to research this
+              // ball.physicsMotion = PhysicsMotionComponent()
+              // add gravity
+              ball.components[PhysicsBodyComponent.self]?.isAffectedByGravity = true
+              // editor - need to research this.
+              //              let visDebug = ModelDebugOptionsComponent(visualizationMode: .textureCoordinates)
+              //              ball.modelDebugOptions = visDebug
+              content.add(ball)
+            }
+        }
+        .gesture(dragGesture)
+    }
+  var dragGesture: some Gesture {
+    DragGesture()
+      .targetedToAnyEntity()
+        .onChanged { value in
+          value.entity.position = value.convert(value.location3D, from: .local, to: value.entity.parent!)
+          value.entity.components[PhysicsBodyComponent.self]?.mode = .kinematic
+        }
+        .onEnded { value in
+          value.entity.components[PhysicsBodyComponent.self]?.mode = .dynamic
+         }
+  }
+}
+
+#Preview {
+    ImmersiveView()
+        .previewLayout(.sizeThatFits)
+}
